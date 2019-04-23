3c87c4351d956e84123066b6429b83c6a5a685ba
Ілля Слівчук <illyasliva433456@ukr.net>
2019-04-23 19:57:34 +0300

    Add project files.

---
+++ RequireConfirmedEmail/Data/Migrations/20181219234402_UnconfirmedEmail.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RequireConfirmedEmail.Data.Migrations
+{
+    public partial class UnconfirmedEmail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UnconfirmedEmail",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UnconfirmedEmail",
+                table: "AspNetUsers");
+        }
+    }
+}
---
+++ RequireConfirmedEmail/Entities/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RequireConfirmedEmail.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [PersonalData]
+        public string UnconfirmedEmail { get; set; }
+    }
+}
